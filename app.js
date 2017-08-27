abfc6a6ed8ce5c507a2563f1653139a79dbd7fb4
Crystal Wesnoski <crystalm@fb.com>
2017-08-27 13:31:37 -0700

    fixing gmail permissions

--- app.js
+++ app.js
@@ -159,11 +159,10 @@ app.get('/auth/github/callback', passport.authenticate('github', { failureRedire
   res.redirect(req.session.returnTo || '/');
 });
 app.get('/auth/google', passport.authenticate('google',
-    { scope: [ 'https://www.googleapis.com/auth/gmail.compose'
-                //'https://www.googleapis.com/auth/userinfo.email',
-                //'https://www.googleapis.com/auth/userinfo.profile',
-                //'https://www.googleapis.com/auth/gmail.compose',
-                //'https://www.googleapis.com/auth/gmail.send',
+    { scope: [ 'https://www.googleapis.com/auth/gmail.compose',
+                'https://www.googleapis.com/auth/userinfo.email',
+                'https://www.googleapis.com/auth/userinfo.profile',
+                'https://www.googleapis.com/auth/gmail.send'
                 //'https://mail.google.com/',
                 //'https://www.googleapis.com/auth/gmail.readonly',
               ]
