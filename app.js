11d30c7f99175e08cd6a0d77de23ce279c82e3cf
Crystal Wesnoski <crystalm@fb.com>
2017-08-27 13:27:40 -0700

    fixing gmail permissions

--- app.js
+++ app.js
@@ -159,12 +159,14 @@ app.get('/auth/github/callback', passport.authenticate('github', { failureRedire
   res.redirect(req.session.returnTo || '/');
 });
 app.get('/auth/google', passport.authenticate('google',
-    { scope: [  'https://www.googleapis.com/auth/gmail.readonly',
-                'https://www.googleapis.com/auth/userinfo.email',
-                'https://www.googleapis.com/auth/userinfo.profile',
-                'https://www.googleapis.com/auth/gmail.compose',
-                'https://www.googleapis.com/auth/gmail.send',
-                'https://mail.google.com/']
+    { scope: [ 'https://www.googleapis.com/auth/gmail.compose'
+                //'https://www.googleapis.com/auth/userinfo.email',
+                //'https://www.googleapis.com/auth/userinfo.profile',
+                //'https://www.googleapis.com/auth/gmail.compose',
+                //'https://www.googleapis.com/auth/gmail.send',
+                //'https://mail.google.com/',
+                //'https://www.googleapis.com/auth/gmail.readonly',
+              ]
     }));
 app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
   res.redirect(req.session.returnTo || '/order');
