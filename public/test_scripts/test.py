3667289a4959386c657eea6ad9f39b5b2f8bb918
Crystal Wesnoski <crystalm@fb.com>
2017-09-09 15:16:59 -0700

    fixing requirements file

--- public/test_scripts/test.py
+++ public/test_scripts/test.py
@@ -23,8 +23,8 @@ from pymongo import MongoClient
 local_host = 'mongodb://localhost:27017/test'
 uri = 'mongodb://crystalrood:Crystal1992@ds161503.mlab.com:61503/heroku_4jtg3rvf'
 
-connection = pymongo.MongoClient(ds161503.mlab.com, 61503)
-db = connection['test']
+connection = pymongo.MongoClient('ds161503.mlab.com', 61503)
+db = connection['heroku_4jtg3rvf']
 db.authenticate('crystalrood', 'Crystal1992')
 
 
