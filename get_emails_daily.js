ad4675bb767d5d5b4f00a9f0a9a041f467770a3e
Crystal Wesnoski <crystalm@fb.com>
2017-09-24 14:44:35 -0700

    changing lookback

--- get_emails_daily.js
+++ get_emails_daily.js
@@ -238,10 +238,8 @@ function sayHello() {
 
                         console.log('made it here')
 
-                        email_thread.save((err) => {
-                          if (err) { return next(err);}
-                          console.log('saved thread')  
-                        });
+                        email_thread.save();
+                        
                         j++;
                        
                         console.log(j)
