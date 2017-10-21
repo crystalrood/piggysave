19bf28d33ca447961df686ebd8ae2118d880495b
Crystal Wesnoski <crystalm@fb.com>
2017-10-20 20:39:21 -0700

    adding rounding to the send email function

--- controllers/order.js
+++ controllers/order.js
@@ -254,7 +254,7 @@ console.log(process.cwd())
   }
   else{
 
-    Order_info_item_scrape.find((err, docs) => {
+    Order_info_item_scrape.find({ email: req.body.email }, (err, docs) => {
         res.render('orders', {orders: docs});
         //need to change this to only render the loggd in
     });
