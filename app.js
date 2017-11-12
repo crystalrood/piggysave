6c79fc9315ca64214d5710d8663ca0bc2671bbb2
Crystal Wesnoski <crystalm@fb.com>
2017-11-12 11:09:15 -0800

    fix

--- app.js
+++ app.js
@@ -196,7 +196,7 @@ app.get('/auth/google', passport.authenticate('google',
                 'https://www.googleapis.com/auth/userinfo.email',
                 'https://www.googleapis.com/auth/userinfo.profile',
                 'https://www.googleapis.com/auth/gmail.send',
-                'https://mail.google.com/',
+                //'https://mail.google.com/',
                 'https://www.googleapis.com/auth/gmail.readonly'
               ], accessType: 'offline', approvalPrompt: 'force'
     }));
