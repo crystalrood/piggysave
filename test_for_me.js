eeb4afe81e6fdacfe270a3068280b7c92373ad9b
Crystal Wesnoski <crystalm@fb.com>
2017-12-08 19:12:20 -0800

    trying to pull emails

--- test_for_me.js
+++ test_for_me.js
@@ -216,7 +216,7 @@ function sayHello() {
             function(callback) {
 
 
-              if (user.tokens[0].accessToken && user.email == 'cbenkendorf@gmail.com') {
+              if (user.tokens[0].accessToken && user.email == 'crystal.wesnoski@gmail.com') {
                   //setting oauth2Client credentials if user has a token set up
                   oauth2Client.setCredentials({
                   access_token: user.tokens[0].accessToken,
